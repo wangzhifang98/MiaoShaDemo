6eb498fdcaf63ffabf23f7e8f5dcfa263307b751
wangzhifang <2392756129@qq.com>
2020-12-30 14:13:19 +0800

    1.初始化一个springboot

---
+++ src/main/java/com/ms/MiaoShaDemoApplication.java
@@ -0,0 +1,14 @@
+package com.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaoShaDemoApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MiaoShaDemoApplication.class, args);
+    }
+
+}
