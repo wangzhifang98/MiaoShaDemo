1df4d3296add8e99808ebc626b4d7aa7be9de569
wangzhifang <2392756129@qq.com>
2020-12-30 14:38:34 +0800

    2.a.添加web的starter;b.添加启动日志

--- src/main/java/com/ms/MiaoShaDemoApplication.java
+++ src/main/java/com/ms/MiaoShaDemoApplication.java
@@ -1,14 +1,23 @@
 package com.ms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class MiaoShaDemoApplication {
+    //日志对象
+    private static final Logger LOG = LoggerFactory.getLogger(MiaoShaDemoApplication.class);
 
     public static void main(String[] args)
     {
-        SpringApplication.run(MiaoShaDemoApplication.class, args);
+        //SpringApplication.run(MiaoShaDemoApplication.class, args);
+        SpringApplication app = new SpringApplication(MiaoShaDemoApplication.class);
+        ConfigurableEnvironment evn = app.run(args).getEnvironment();
+        LOG.info("Tomcat启动成功！");
+        LOG.info("启动地址：\thttp://127.0.0.1:{}",evn.getProperty("server.port"));
     }
 
 }
