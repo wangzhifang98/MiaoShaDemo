cb99c20d12157c2b9d05063b01f649f74b942af9
wangzhifang <2392756129@qq.com>
2021-01-06 14:15:50 +0800

    1.controller成功生成验证码与手机号相关联

--- src/main/java/com/ms/controller/UserController.java
+++ src/main/java/com/ms/controller/UserController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
 /**
  * @author 王智芳
  * @data 2020/12/30 16:10
@@ -19,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController extends BaseController{
     @Autowired
     private UserService userService;
+    @Autowired
+    private HttpServletRequest httpServletRequest;
 
     @RequestMapping("/get")
     public CommonReturnType getUser(@RequestParam(name = "id")Integer id) throws BussinessException {
@@ -42,5 +47,22 @@ public class UserController extends BaseController{
         UserVO userVO = new UserVO();
         BeanUtils.copyProperties(userModel,userVO);
         return userVO;
+    }
+
+    //用户获取otp短信接口
+    @RequestMapping("/getotp")
+    public CommonReturnType getOtp(@RequestParam(name="telphone")String telphone){
+        //需要按照一定规则生成OTP验证码
+        Random random = new Random();
+         int randomInt= random.nextInt(99999);
+         randomInt+=100000;
+         String otpCode = String.valueOf(randomInt);
+
+        //将OTP验证码同对应用户的手机号关联
+        httpServletRequest.getSession().setAttribute(telphone,otpCode);
+        System.out.printf("telphone = %s & otpCode = %s%n", telphone, otpCode);
+        //将OTP验证码通过短信通道发送给用户,省略
+
+        return CommonReturnType.create(null);
     }
 }
