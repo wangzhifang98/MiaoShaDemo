62bb42c8a455d385483a1ff0cd80587be5a30cb8
wangzhifang <2392756129@qq.com>
2021-01-08 10:11:17 +0800

    1.编写model,根据model设计表，在用生成器生成do(item,itemstock)成功

---
+++ src/main/java/com/ms/dao/ItemDOMapper.java
@@ -0,0 +1,17 @@
+package com.ms.dao;
+
+import com.ms.dataobject.ItemDO;
+
+public interface ItemDOMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ItemDO record);
+
+    int insertSelective(ItemDO record);
+
+    ItemDO selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ItemDO record);
+
+    int updateByPrimaryKey(ItemDO record);
+}
